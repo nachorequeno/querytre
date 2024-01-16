6146da24c119f38339e5697230fdbe018c130773
Ignacio Requeno <ppnacho85@gmail.com>
2024-01-16 18:25:04 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='querytre',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A library for querying Timed Regular Expressions (TRE)',
+    author='Akshay Mambakam',
+)
