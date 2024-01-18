8c5a674a16314ddd463b1a4d45c0578da5de8de3
Ignacio Requeno <ppnacho85@gmail.com>
2024-01-18 15:59:46 +0100

    Trying to fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages, Extension, setup
+
+ext_instance_int = Extension(
+    'ext_int',
+    sources=['timedrel/boost/timedrel_ext_int.cpp'],
+    include_dirs=['include'],
+    libraries=['boost_python3'],
+)
+
+ext_instance_float = Extension(
+    'ext_float',
+    sources=['timedrel/boost/timedrel_ext_float.cpp'],
+    include_dirs=['include'],
+    libraries=['boost_python3'],
+)
 
 setup(
     name='querytre',
@@ -6,4 +20,9 @@ setup(
     version='0.1.0',
     description='A library for querying Timed Regular Expressions (TRE)',
     author='Akshay Mambakam',
+    license='GPLv3+',
+    python_requires='>=3',
+    install_requires=['antlr4-python3-runtime>=4.13'],
+    ext_package='timedrel',
+    ext_modules=[ext_instance_int, ext_instance_float]
 )
