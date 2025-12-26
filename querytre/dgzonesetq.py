1769eb5b2bb0c2392ae577c8601161fce887602c
akshaymambakam <akshaymambakam@gmail.com>
2025-12-26 14:46:48 +0530

    [PARAMID] Infer duration parameters with diagnostics.

--- querytre/dgzonesetq.py
+++ querytre/dgzonesetq.py
@@ -5,6 +5,8 @@ from matplotlib.figure import Figure
 import io
 from PIL import Image
 from anytree import Node, RenderTree
+from fractions import Fraction
+from ppl import Variable, Constraint_System, C_Polyhedron
 
 class diagtree(object):
     """Diagnostics tree for TRE"""
@@ -34,13 +36,15 @@ class diagtree(object):
 class dgzonesetq(object):
     """Python wrapper for dgzone_set C++ class"""
 
-    def __init__(self, zsq, notation="", children=None, op_type="atomic"):
+    def __init__(self, zsq, notation="", children=None, op_type="atomic", timing_parameters=None):
         """Initialize the dgzone_set object.
 
         Args:
             data: Rational zone set from which the dgzone_set will be created.
             notation (str, optional): The notation associated with the dgzone_set.
             children (list, optional): The list of children in the parse tree.
+            op_type (str): The type of operator denoted with a string
+            timing_parameters [list, optional]: The timing parameters
         """
         if children is None:
             children = []
@@ -55,6 +59,10 @@ class dgzonesetq(object):
 
         self.children = children
         self.op_type = op_type
+        if timing_parameters is None:
+            self.timing_parameters = []
+        else:
+            self.timing_parameters = timing_parameters
 
     def __and__(self, other):
         """Intersection operator (&) for dgzone_set."""
@@ -99,10 +107,10 @@ class dgzonesetq(object):
         kplus_container = ext.dgzone_set.kleene_plus(self.container)
         return dgzonesetq(kplus_container, children=[self], op_type="kplus")
 
-    def duration_restriction(self, dmin, dmax):
+    def duration_restriction(self, dmin, dmax, timing_parameters=None):
         """Apply duration restriction on the dgzone_set."""
         dr_container = ext.dgzone_set.duration_restriction(self.container, dmin, dmax)
-        return dgzonesetq(dr_container, children=[self], op_type="durarest")
+        return dgzonesetq(dr_container, children=[self], op_type="durarest", timing_parameters=timing_parameters)
 
     def infer_concatenation(self, index, dg1, dg2, time_interval):
         """Infer concatenation for a dgzone_set."""
@@ -153,3 +161,59 @@ class dgzonesetq(object):
                     diag_children.append(child_tree)
 
         return diagtree(time_interval, notation, diag_children)
+
+    # [TODO]? Can this function be written over the diagtree?
+    def infer_parameter_constraints(self, index, time_interval):
+        notation = self.notation
+
+        child_tintervals = self.infer(index, time_interval)
+        child_zindices = self.child_zone_indices(index)
+        assert len(child_tintervals) == len(child_zindices)
+
+        # [TODO]? Using Python Fractions instead of gmp rationals
+        # The parameter constraints list to return
+        parameter_constraints = []
+
+        if self.op_type == "durarest":
+            assert len(self.timing_parameters) <= 2
+            assert len(time_interval) == 2
+            i = 0
+            interval_duration = Fraction(time_interval[1]) - Fraction(time_interval[0])
+            duration_numerator = interval_duration.numerator
+            duration_denominator = interval_duration.denominator
+            if len(self.timing_parameters) == 1:
+                parameter = self.timing_parameters[0]
+                # This is the lower bound
+                constraint = parameter*duration_denominator <= duration_numerator
+                parameter_constraints.append(constraint)
+
+                # This is the upper bound
+                constraint = duration_numerator <= duration_denominator*parameter
+                parameter_constraints.append(constraint)
+            elif len(self.timing_parameters) == 2:
+                for parameter in self.timing_parameters:
+                    if i == 0:
+                        # This is the lower bound parameter
+                        constraint = parameter*duration_denominator <= duration_numerator
+                        parameter_constraints.append(constraint)
+                    elif i == 1:
+                        # This is the upper bound parameter
+                        constraint = duration_numerator <= duration_denominator*parameter
+                        parameter_constraints.append(constraint)
+                    i += 1
+
+            for czindex, ctinterval, child in zip(child_zindices, child_tintervals, self.children):
+                if czindex >= 0:
+                    child_parameter_constraints = child.infer_parameter_constraints(czindex, ctinterval)
+                    parameter_constraints += child_parameter_constraints
+        elif self.op_type == "kplus":
+            for czindex, ctinterval in zip(child_zindices, child_tintervals):
+                child_parameter_constraints = self.children[0].infer_parameter_constraints(czindex, ctinterval)
+                parameter_constraints += child_parameter_constraints
+        else:
+            for czindex, ctinterval, child in zip(child_zindices, child_tintervals, self.children):
+                if czindex >= 0:
+                    child_parameter_constraints = child.infer_parameter_constraints(czindex, ctinterval)
+                    parameter_constraints += child_parameter_constraints
+
+        return parameter_constraints
