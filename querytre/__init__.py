962228a2d5e8e9bc2098c7cb18cb5ccb20272c76
José Ignacio Requeno <jrequeno@ucm.es>
2026-01-28 10:40:04 +0100

    Updating __init__.py in queryTRE

--- querytre/__init__.py
+++ querytre/__init__.py
@@ -2,3 +2,7 @@ from .zoneset import zoneset
 from .zonesetf import zoneset as zonesetf
 from .zonesetq import zoneset as zonesetq
 from .dgzonesetq import dgzonesetq
+
+# __version__ = '0.1.0'
+# __name__ = 'querytre'
+# __all__ = ['zoneset', 'zonesetf', 'zonesetq', 'dgzonesetq']
